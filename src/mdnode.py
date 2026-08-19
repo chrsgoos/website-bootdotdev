905f22f12638e1886262c76416781b438a38dc38
Christian Goos <chris.goos@posteo.de>
2026-08-19 15:40:29 +0200

    c3 / l1 unfinished pt.3

--- src/mdnode.py
+++ src/mdnode.py
@@ -1,16 +1,20 @@
 from textnode import TextType, TextNode
 
-def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType) -> list[TextNode]:
+
+def split_nodes_delimiter(
+    old_nodes: list[TextNode], delimiter: str, text_type: TextType
+) -> list[TextNode]:
     new_nodes: list[TextNode] = []
-    
+
     for node in old_nodes:
-        if not node.text_type.TEXT:
-            new_nodes.extend(node)
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
 
         if node.text.count(delimiter) % 2 != 0:
             raise Exception(f"{delimiter} was not closed")
 
-        text_split: list = node.text.split(delimiter, maxsplit=2)
+        text_split: list = node.text.split(delimiter)
         new_nodes.append(TextNode(text_split[0], TextType.TEXT))
 
         match delimiter:
@@ -20,6 +24,5 @@ def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: 
                 new_nodes.append(TextNode(text_split[1], TextType.ITALIC))
             case "`":
                 new_nodes.append(TextNode(text_split[1], TextType.CODE))
-
 
     return new_nodes
